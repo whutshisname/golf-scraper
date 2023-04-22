aa7eb2892e9cb9d25dc4a59ecb7a5472692da45b
Brian Gray <brian.gray@whutshisname.com>
2023-04-22 17:47:12 -0500

    attempting to fix chromium error

--- src/pages/api/uniqueShaftTypes.js
+++ src/pages/api/uniqueShaftTypes.js
@@ -1,5 +1,5 @@
-// src/pages/api/uniqueShaftTypes.js
 import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
 
 export default async function handler(req, res) {
   const pageUrl = req.query.pageUrl;
@@ -9,7 +9,12 @@ export default async function handler(req, res) {
     return;
   }
 
-  const browser = await puppeteer.launch({ headless: true }); 
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    executablePath: await chromium.executablePath,
+    headless: true,
+  });
+
   const page = await browser.newPage();
   await page.goto(pageUrl);
 
